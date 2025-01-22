199a7ddcc7efd8084dfdb56a84198f7ddbffb38c
CibelleN94 <belles2.n@gmail.com>
2025-01-22 13:49:59 -0300

    atualizando com novos exercicios

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -1,8 +1,7 @@
-var pais = 'França'
-console.log (`vivendo em ${pais}`)
-if (pais == 'Brasil') {
-    console.log (`Você é brasileiro`)
-} else {
-    console.log (`Você é estrangeiro`)
+var vel = 90
+console.log (`A sua velocidade é de ${vel}`)
+if (vel > 60) {
+    console.log (`Você ultrapassou a velocidade. Multado!`)
 }
 
+console.log (`Use o cinto de segurança e dirija com cuidado!`)
---
+++ aula12/ex011.js
@@ -0,0 +1,8 @@
+var idade = 15
+console.log(`Você tem ${idade}.`)
+if (idade < 18|| idade > 65 ) {
+    console.log (`Voto opcional.`) }
+
+    else {
+        console.log (`Voto obrigatório.`)
+    }
