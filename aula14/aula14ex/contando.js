92e7cbf4c5d2c5696dab5c5c0ad79ec7b1f51f5d
CibelleN94 <belles2.n@gmail.com>
2025-02-01 20:49:14 -0300

    tabuada

--- aula14/aula14ex/contando.js
+++ aula14/aula14ex/contando.js
@@ -1,18 +1,22 @@
-function contar() {    
-    var n1 = document.querySelector('input#num1')
-    var n2 = document.querySelector('input#num2')
-    var pass = document.querySelector('input#pass')
-    var res = document.querySelector('div#res')
-    
-if (n1.value.length == 0 || n2.value.length == 0 || pass.value.length == 0) {
-    window.alert ('[ERRO] Faltam dados.')
-} else {
-    res.innerHTML = 'Contando:'
-    var i = Number(n1.value)
-    var f = Number (n2.value)
-    var passo = Number (pass.value)
+    function contar() {    
+        let n1 = document.querySelector('input#num1')
+        let n2 = document.querySelector('input#num2')
+        let pass = document.querySelector('input#pass')
+        let res = document.querySelector('div#res')
 
-    for (var c = i; i <=f; c+=passo)
-        res.innerHTML = `${c}`
+    if (n1.value.lenght == 0|| n2.value.lenght == 0 || pass.value.lenght == 0) 
+    {
+        window.alert ('[ERRO] Faltam dados')
+    } else {
+        res.innerHTML = 'Contando...'
+        let i = Number(n1.value)
+        let f = Number(n2.value)
+        let p = Number(pass.value)
+
+        for(let c = i; c <= f; c += p){
+            res.innerHTML += ` ${c} `
+        }
+    }
 }
-}
+       
+    
